33f01ff3ff921fa9e050f5fc99fd2b939be0b1f2
Shivam Rathore <shivamrathore307@gmail.com>
2020-03-17 12:45:28 +0530

    rating and voting added

--- zomato_scrapper.py
+++ zomato_scrapper.py
@@ -2,15 +2,18 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import re
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 url='https://www.zomato.com/bangalore/south-bangalore-restaurants?page='
 
-pages=[1,3,4,5,6]
-records=[]
-restaurant_id=0
-
 #extracting information from the pages
+pages=[1,2,3,4,5,6]
+
+records=[]
+res_id=0
+ress=[]
+
 for page in pages:
 
     #extracting the content of the url
@@ -19,36 +22,57 @@ for page in pages:
     #parsing the content by beautifulsoup
     soup = BeautifulSoup(res.text, 'html.parser')
 
-    #extracting all restaurants of class 'pos-relative clearfix'
+    #extracting all restaurants
     restaurants = soup.find_all("div",attrs={"class": "pos-relative clearfix"})
-
+    
     for i in range(len(restaurants)):
         #extracting restaurant id
-        restaurant_id+=1  
+        res_id+=1
 
-        #extracting area
-        area=restaurants[i].find("a",attrs={'class':'ln24 search-page-text mr10 zblack search_result_subzone left'}).text
+        #extracting restaurant area
+        res_area=restaurants[i].find("a",attrs={'class':'ln24 search-page-text mr10 zblack search_result_subzone left'}).text
         
         #extracting restaurant type
-        restaurant_type= restaurants[i].find("div",attrs={"class": "res-snippet-small-establishment mt5"}).text.rstrip()
+        res=restaurants[i].find_all("a",attrs={"class": "zdark ttupper fontsize6"})
+        res_type=''
+        
+        for inn in res:
+            res_type+=(inn.text+',')
+        
+        res_type=res_type[:-1]
         
         x=restaurants[i].contents[1].find_all("a")
         cnt=0
-
-        for i in restaurant_type:
-            if i==',':
+        for ind in res_type:
+            if ind==',':
                 cnt+=1
-        name=x[cnt+2].text.rstrip()
+        res_name=x[cnt+2].text.rstrip()
         
-        #compiling all the tuples
-        records.append((restaurant_id,name, area, restaurant_type))
+        ##extracting rating of restaurant
+        rating_pattern= r"rating-popup rating-for-(\d+)[\w -]+(\d)"
+        ress = re.search(rating_pattern, str(restaurants[i]))
+        if ress is not None:
+            rating=float(restaurants[i].find("div", attrs={"class": "rating-popup rating-for-"+ress[1]+" res-rating-nf right level-"+ress[2]+" bold"}).text)
+        else:
+            rating=None
 
-    #dataframes    
-    df=pd.DataFrame(records, columns=['restaurant_id','name', 'area', 'restaurant_type'])
+        #extracting voting of restaurant
+        voting_pattern= r"rating-votes-div-(\d+)[\w ->]+\>(\d+)"
+        vote_obj =re.search(voting_pattern, str(restaurants[i]))
+        if vote_obj is not None:
+            voting=vote_obj[2]
+        else:
+            voting=None
+        
+        #compiling all the tuples for exactly 80 restaurants
+        if res_id<=80:
+            records.append((res_id,res_name, res_area, res_type, rating, voting))
+
+#dataframes
+df=pd.DataFrame(records, columns=['res_id','res_name', 'res_area', 'res_type', 'rating', 'voting'])
 
 #exporting to json format
 df.to_json('output.json', orient='records', lines=True)
 
 #exporting to csv format
-df.to_csv('zomato_restaurants.csv',index=False) 
-
+df.to_csv('zomato_restaurants.csv',index=False)
